975dd5d136403fecbdb2d4680c31085b9bab9544
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2024-02-20 16:54:07 +0100

    session 4

--- Day2/Session3/src/main.cpp
+++ Day2/Session3/src/main.cpp
@@ -15,3 +15,62 @@
 // [ ] Make a countdown: have the numbers count down to zero.
 // [ ] Restart the countdown every time a button is pressed.
 
+#include <Arduino.h>
+
+void show0(){
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+}
+
+void show1(){
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,HIGH);
+}
+
+void show2(){
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+}
+
+
+void setup() {
+  int segmentPins[] = {2,3,6,7,8,9,10,11,12}; // define pins to set as outputs
+  
+  for (int i = 0; i < 9; i++){ 
+    pinMode(segmentPins[i], OUTPUT);
+  }
+  digitalWrite(2,HIGH); // set pin 2 as HIGH to turn on 1st digit
+  digitalWrite(3,HIGH); // set pin 2 as HIGH to turn on 1st digit
+  for (int i = 2; i < 10; i++){ 
+    digitalWrite(segmentPins[i], LOW);
+  }
+}
+void loop() {
+    show0();
+    delay(1000);
+    show1();
+    delay(1000);
+    show2();
+    delay(1000);
+}
